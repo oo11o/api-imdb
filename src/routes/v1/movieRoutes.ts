2c3de7fd393fdf8c287fae99b55bb0e7a3ba44fb
Volodymyr <ymyr.vlad@gmail.com>
2025-01-12 19:11:55 +0200

    [IA-18] Add global error handler in index.ts

--- src/routes/v1/movieRoutes.ts
+++ src/routes/v1/movieRoutes.ts
@@ -1,15 +1,19 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { ErrorResponseDTO } from '../../dto/responses/ErrorRespomseDTO.js';
 import { MovierAdapter } from '../../parser/MovierAdapter.js';
 
 const router = Router();
 
-router.get('/movies/:id', async (req: Request, res: Response) => {
+const asyncHandler = (fn: Function) => (req: Request, res: Response, next: NextFunction) =>
+    Promise.resolve(fn(req, res, next)).catch(next);
+
+router.get('/movies/:id', asyncHandler(async (req: Request, res: Response) => {
     const test = new MovierAdapter();
     const { id } = req.params;
 
     res.json(await test.getDetailsByIMDBId(id));
-});
+}));
+
 
 router.get('/movies', (req: Request, res: Response) => {
     const errorResponse: ErrorResponseDTO = {
